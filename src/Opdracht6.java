21629d7d223ee4f3d80bcf8d1ab3ebb5eedae9b9
AxelB0 <axel.boonen.mail@gmail.com>
2022-03-21 08:54:35 +0100

    edit

--- src/Opdracht6.java
+++ src/Opdracht6.java
@@ -2,13 +2,12 @@ public class Opdracht6 {
     public static void main(String[] args){
 
         int number1 = 1, number2 = 2;
-        System.out.println("< "+(number1<number2));
-        System.out.println("<= "+(number1<=number2));
-        System.out.println("> "+(number1>number2));
-        System.out.println(">= "+(number1>=number2));
-        System.out.println("== "+(number1==number2));
-        System.out.println("!= "+(number1!=number2));
+        System.out.println("1<2 "+(number1<number2));
+        System.out.println("1<=2 "+(number1<=number2));
+        System.out.println("1>2 "+(number1>number2));
+        System.out.println("1>=2 "+(number1>=number2));
+        System.out.println("1==2 "+(number1==number2));
+        System.out.println("1!=2 "+(number1!=number2));
 
     }
-
 }
